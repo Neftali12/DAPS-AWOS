6b3306428e9c9349e1b148e09d98bb4c55004611
Neftali12 <neftiie.carranza@gmail.com>
2020-12-04 13:57:11 -0600

    Creada la cadena de conexion con MONGO ATLAS en la nube

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(require('./routes/login.js'));
 app.use(require('./routes/categoria.js'));
 app.use(require('./routes/producto.js'));
 
-mongoose.connect('mongodb://localhost:27017/cafeteria', {
+mongoose.connect('mongodb+srv://admin:administrador15@cluster0.29r8p.mongodb.net/cafeteria', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
