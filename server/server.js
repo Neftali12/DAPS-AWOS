2dc6d2fc38e694a01c000076e2644d318ec1fa29
Neftali12 <neftiie.carranza@gmail.com>
2020-12-10 05:24:52 -0600

    PROYECTO TERMINADO

--- server/server.js
+++ server/server.js
@@ -1,33 +1,21 @@
 require('./config/config');
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
+var bodyParser = require('body-parser');
 // Habilita CORS
 
 app.use((req, res, next) => {
-
     res.setHeader('Access-Control-Allow-Origin', '*');
-
     res.setHeader(
-
         'Access-Control-Allow-Headers',
-
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-
     );
-
     res.setHeader(
-
         'Access-Control-Allow-Methods',
-
         'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-
     );
-
     next();
-
 });
 
 // parse application/x-www-form-urlencoded
@@ -40,10 +28,11 @@ app.get('/', function(req, res) {
     res.send('<h1> Bienvenido a mi servidor REST (localhost) </h1>');
 });
 
-app.use(require('./routes/usuario.js'));
-app.use(require('./routes/login.js'));
-app.use(require('./routes/categoria.js'));
-app.use(require('./routes/producto.js'));
+app.use(require('./routes/usuario'));
+app.use(require('./routes/categoria'));
+app.use(require('./routes/producto'));
+app.use(require('./routes/login'));
+
 
 mongoose.connect('mongodb+srv://admin:administrador15@cluster0.29r8p.mongodb.net/cafeteria', {
     useNewUrlParser: true,
