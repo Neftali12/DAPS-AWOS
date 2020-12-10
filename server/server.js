d46366334848bb25eb1238786ba21984716a4fb4
Neftali12 <neftiie.carranza@gmail.com>
2020-12-10 03:44:21 -0600

    FINAL

--- server/server.js
+++ server/server.js
@@ -2,33 +2,33 @@ require('./config/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 // Habilita CORS
 
-// app.use((req, res, next) => {
+app.use((req, res, next) => {
 
-//     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-//     res.setHeader(
+    res.setHeader(
 
-//         'Access-Control-Allow-Headers',
+        'Access-Control-Allow-Headers',
 
-//         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 
-//     );
+    );
 
-//     res.setHeader(
+    res.setHeader(
 
-//         'Access-Control-Allow-Methods',
+        'Access-Control-Allow-Methods',
 
-//         'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+        'GET, POST, PATCH, PUT, DELETE, OPTIONS'
 
-//     );
+    );
 
-//     next();
+    next();
 
-// });
+});
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
